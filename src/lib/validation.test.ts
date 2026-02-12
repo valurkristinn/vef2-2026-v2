4e594c859354c1d7034ac3bdd2af6a818f06cede
valurkristinn <109678364+valurkristinn@users.noreply.github.com>
2026-02-12 10:16:19 +0000

    added one test

--- src/lib/validation.test.ts
+++ src/lib/validation.test.ts
@@ -1,12 +1,13 @@
 import assert from 'node:assert';
 import { describe, it } from 'node:test';
+import { todoSchema } from './validation.js';
 
 describe('parse', () => {
   describe('parseTodo', () => {
-    it('should parse text that contains , correctly', () => {
-      const line = '2,Stjörnufræði,2,,"Oberon, Titania og Puck eru tungl hvaða plánetu",Úranusar'
-      const result = parseLine(line);
-      assert.strictEqual(result.question, "Oberon, Titania og Puck eru tungl hvaða plánetu")
+    it('should parse todo with a id as number, title as non-empty string and finished as boolean', () => {
+      const todo = {id: 1, title: "test", finished: "on"}
+      const result = todoSchema.safeParse(todo)
+      assert(result.success)
     })
   })
 });
